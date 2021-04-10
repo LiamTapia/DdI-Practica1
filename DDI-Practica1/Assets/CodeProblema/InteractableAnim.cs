6d8b70707eea15c45ad08eee910d2879a6de39f8
LiamTapia <36176046+LiamTapia@users.noreply.github.com>
2021-04-09 23:44:47 -0700

    Practica 6.2

---
+++ DDI-Practica1/Assets/CodeProblema/InteractableAnim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableAnim : MonoBehaviour 
+{
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void UpdateAnimation(int option)
+    {
+        switch(option){
+            case 1:
+                _animator.SetBool("Walk",true);
+                break;
+            case 2:
+                _animator.SetBool("Walk",false);
+                break;
+        }
+
+    }
+}
