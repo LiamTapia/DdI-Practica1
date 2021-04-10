24cdc2d24d0a71ac765ac3fc1696c76e8cffbf43
LiamTapia <36176046+LiamTapia@users.noreply.github.com>
2021-04-09 23:47:16 -0700

    Practica 6.2

---
+++ DDI-Practica1/Assets/CodeProblema/VirtualButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public InteractableAnim animObject;
+    private VirtualButtonBehaviour virtualButton;
+    public int option = 1;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        animObject.UpdateAnimation(option);
+        Debug.Log("Se presiono");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Se levanto");
+    }
+
+    void Awake()
+    {
+        virtualButton = GetComponent<VirtualButtonBehaviour>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(virtualButton != null)
+        {
+            virtualButton.RegisterEventHandler(this);
+        }
+    }
+
+    
+}
